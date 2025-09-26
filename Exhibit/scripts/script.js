82eaf192c66dd04923b35d79013c60787348ee6c
Korphic <kierluna321@gmail.com>
2025-09-26 17:46:46 +0800

    Add files via upload

---
+++ Exhibit/scripts/script.js
@@ -0,0 +1,7 @@
+let isUserReady = confirm("Hello! Welcome to my Exhibit. Would you like to continue?");
+console.log(isUserReady);
+if (isUserReady) {
+    alert("Welcome to my Exhibit!");
+} else {
+    alert("Well thats too bad your here now.. Enjoy anyways!");
+}
